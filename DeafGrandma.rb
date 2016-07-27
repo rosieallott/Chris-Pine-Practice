09ee8b3a683c14195053ec6ee55e8309f887108d
Rosie Allott <rosie.allott@gmail.com>
2016-07-27 10:19:35 +0100

    Chapter 7 Deaf Grandma incl extension

--- DeafGrandma.rb
+++ DeafGrandma.rb
@@ -1,10 +1,40 @@
+puts "first solution for extension"
+
 puts "Say something to Grandma"
+bye_count=0
+
 while true
   answer=gets.chomp
   if answer=="BYE"
-    puts "enough now"
-    break
+    bye_count+=1
   else
-    puts answer==answer.upcase ? "NO, NOT SINCE #{rand(1930...1950)}!" : "HUH?! SPEAK UP SONNY!"
+    bye_count=0
   end
+
+  if bye_count>=3
+    puts "ok bye now"
+    break
+  end
+
+  if answer==answer.upcase
+    puts "NO, NOT SINCE #{rand(1930...1950)}!"
+  else
+    puts "HUH?! SPEAK UP SONNY!"
+  end
+end
+
+puts "---------"
+puts "more elegant solution"
+puts "--------"
+
+puts "Say something to Grandma"
+bye_count=0
+while true
+  answer=gets.chomp
+  answer=="BYE" ? bye_count+=1 : bye_count=0
+    if bye_count>=3
+      puts "ok bye now"
+      break
+    end
+  puts answer==answer.upcase ? "NO, NOT SINCE #{rand(1930...1950)}!" : "HUH?! SPEAK UP SONNY!"
 end
