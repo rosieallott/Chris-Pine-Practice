305525eac78ab7ff0952bb91f7aca792652659f2
Rosie Allott <rosie.allott@gmail.com>
2016-07-28 13:36:12 +0100

    Ch10 Sorting Algorithm

---
+++ sort_practice.rb
@@ -0,0 +1,17 @@
+def sort array
+  rec_sort array, []
+end
+
+def rec_sort unsorted, sorted
+  return sorted if unsorted.length <=0
+
+  sorted << unsorted.min
+  still_unsorted = []
+  unsorted.delete(unsorted.min)
+  still_unsorted = unsorted
+
+  rec_sort still_unsorted, sorted
+
+end
+
+puts sort ["tar", "rat", "art"]
